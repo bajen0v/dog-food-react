5d23e4816fb45d8c8ac9129b2ce4455398c4733b
bajen0v <ibajenov1991@gmail.com>
2023-03-14 16:06:22 +0400

    create filter search and render card in site

---
+++ src/components/app/index.jsx
@@ -0,0 +1,49 @@
+import { useEffect, useState } from "react";
+import { CardList } from "../card-list";
+import { Footer } from "../footer";
+import { Header } from "../header";
+import { Sort } from "../sort";
+import { Logo } from "../logo";
+import { Search } from "../search";
+import { dataCard } from "../../data";
+import "./styles.css";
+
+export function App() {
+  const [cards, setCards] = useState(dataCard);
+  const [searchQuery, setSearchQuery] = useState("");
+
+  function handleRequest() {
+    const filterCards = dataCard.filter(item => {
+      return item.name.toLowerCase().includes(searchQuery)
+    });
+    setCards(filterCards);
+  }
+
+  function handleFormSubmit(e) {
+    e.preventDefault();
+    handleRequest();
+  }
+
+  function handleInputChange(dataInput) {
+    setSearchQuery(dataInput);
+  }
+
+  useEffect(() => handleRequest(), []);
+
+  return (
+    <>
+      <Header>
+        <Logo />
+        <Search
+          handleFormSubmit={handleFormSubmit}
+          handleInputChange={handleInputChange}
+        />
+      </Header>
+      <main className="content container">
+        <Sort />
+        <CardList goods={cards} />
+      </main>
+      <Footer />
+    </>
+  );
+}
---
+++ src/components/sort/index.jsx
@@ -0,0 +1,13 @@
+import "./styles.css";
+
+export function Sort() {
+  return (
+    <>
+
+    </>
+  )
+}
+
+
+
+
